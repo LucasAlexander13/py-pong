de8fa31abca35b336634ab2cd5f43714a24e7456
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-14 13:47:56 -0300

    Added paddle class and draw_paddle method

---
+++ paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Paddle():
+    def __init__(self, position):
+        self.draw_paddle(position)
+
+    def draw_paddle(self, position):
+        self.paddle = []
+        for i in range(5):
+            segment = Turtle()
+            segment.shape('square')
+            segment.color('white')
+            segment.penup()
+
+            if position == 'right':
+                segment.goto(-220, i * 20)
+                self.paddle.append(segment)
+            
+            elif position == 'left':
+                segment.goto(220, i * -20)
+                self.paddle.append(segment)
