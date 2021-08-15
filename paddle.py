937ae7953b3eb8acba89d19920f9115453e188e7
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-15 15:07:17 -0300

    Paddle position update

--- paddle.py
+++ paddle.py
@@ -9,13 +9,14 @@ class Paddle():
         for i in range(5):
             segment = Turtle()
             segment.shape('square')
+            segment.speed('fastest')
             segment.color('white')
             segment.penup()
 
             if position == 'right':
-                segment.goto(-220, i * 20)
+                segment.goto(360, i * -20)
                 self.paddle.append(segment)
             
             elif position == 'left':
-                segment.goto(220, i * -20)
+                segment.goto(-360, i * 20)
                 self.paddle.append(segment)
