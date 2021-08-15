90371889f75bffdcf5b4e47bf2340dffa0a03129
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-15 17:57:11 -0300

    Added velocity and paddle colide method

--- ball.py
+++ ball.py
@@ -23,22 +23,24 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=0.8)
         self.color('white')
         self.penup()
+        self.velocity = 0.25
         self.set_direction()
     
     def set_direction(self):
+        # added a random ball direction when called
         self.x = choice(('positive', 'negative'))
         self.y = choice(('positive', 'negative'))
     
     def move(self):
         if self.x == 'positive':
-            new_x = self.xcor() + 0.25
+            new_x = self.xcor() + self.velocity
         else:
-            new_x = self.xcor() - 0.25
+            new_x = self.xcor() - self.velocity
         
         if self.y == 'positive':
-            new_y = self.ycor() + 0.25
+            new_y = self.ycor() + self.velocity
         else:
-            new_y = self.ycor() - 0.25
+            new_y = self.ycor() - self.velocity
         self.goto(new_x, new_y)
         self.check_limit()
 
@@ -50,6 +52,12 @@ class Ball(Turtle):
         elif self.ycor() <= -210:
             self.y = 'positive'
             self.pick_color()
+    
+    def paddle_colide(self):
+        if self.x == 'positive':
+            self.x = 'negative'
+        elif self.x == 'negative':
+            self.x = 'positive'
     
     def pick_color(self):
         self.color(choice(color))
