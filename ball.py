d218732f07a9957f915ea04bc6f07eab5b3b1d32
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-15 17:30:28 -0300

    Added color and randomize start direction

--- ball.py
+++ ball.py
@@ -1,4 +1,19 @@
 from turtle import Turtle
+from random import choice
+
+color = [
+    'orange', 
+    'dark orange', 
+    'firebrick', 
+    'dark red', 
+    'crimson', 
+    'medium violet red', 
+    'dark magenta', 
+    'medium orchid', 
+    'medium purple', 
+    'indigo', 
+    'slate blue',
+    ]
 
 class Ball(Turtle):
     def __init__(self,):
@@ -8,9 +23,11 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=0.8)
         self.color('white')
         self.penup()
-
-        self.y = 'positive'
-        self.x = 'positive'
+        self.set_direction()
+    
+    def set_direction(self):
+        self.x = choice(('positive', 'negative'))
+        self.y = choice(('positive', 'negative'))
     
     def move(self):
         if self.x == 'positive':
@@ -28,5 +45,11 @@ class Ball(Turtle):
     def check_limit(self):
         if self.ycor() >= 210:
             self.y = 'negative'
+            self.pick_color()
+
         elif self.ycor() <= -210:
             self.y = 'positive'
+            self.pick_color()
+    
+    def pick_color(self):
+        self.color(choice(color))
