b77ce073915380355959519e15eaf928ba4e7113
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-15 17:19:07 -0300

    Added ball movement

--- ball.py
+++ ball.py
@@ -8,3 +8,25 @@ class Ball(Turtle):
         self.shapesize(stretch_wid=0.8)
         self.color('white')
         self.penup()
+
+        self.y = 'positive'
+        self.x = 'positive'
+    
+    def move(self):
+        if self.x == 'positive':
+            new_x = self.xcor() + 0.25
+        else:
+            new_x = self.xcor() - 0.25
+        
+        if self.y == 'positive':
+            new_y = self.ycor() + 0.25
+        else:
+            new_y = self.ycor() - 0.25
+        self.goto(new_x, new_y)
+        self.check_limit()
+
+    def check_limit(self):
+        if self.ycor() >= 210:
+            self.y = 'negative'
+        elif self.ycor() <= -210:
+            self.y = 'positive'
