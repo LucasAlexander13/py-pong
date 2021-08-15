c15737dc8d977b6d8366823060cfbbcee79cbdde
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-15 15:04:59 -0300

    Added set_screen and draw_edgr functions

--- screen.py
+++ screen.py
@@ -1,7 +1,23 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
-screen.tracer(0)
-screen.screensize(500, 500)
-screen.bgcolor('black')
-screen.title("Pypong")
+
+def set_screen(screen):
+    screen.screensize(800, 500) 
+    screen.bgcolor('black')
+    screen.title('PyPong')
+    screen.tracer(0)
+
+def draw_edge():
+    board = Turtle()
+    board.penup()
+    board.color('firebrick')
+    board.goto(380, 220)
+
+    # draw the edge line of the game
+    board.pendown()
+    board.goto(380, -220)
+    board.goto(-380, -220)
+    board.goto(-380, 220)
+    board.goto(380, 220)
+    board.hideturtle()
